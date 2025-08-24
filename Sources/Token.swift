cadd18c1b7581cd0022d0c5877fa5c20588ad0cf
JL <115737916+yikesboy@users.noreply.github.com>
2025-08-24 15:59:58 +0200

    feat: token struct

---
+++ Sources/Token.swift
@@ -0,0 +1,12 @@
+struct Token {
+    let type: TokenType
+    let lexeme: String
+    let literal: Literal?
+    let line: Int
+}
+
+extension Token: CustomStringConvertible {
+    var description: String {
+        return "\(type) \(lexeme) \(literal)"
+    }
+}
