a077bf05cd363d45ef7f93ece4c72688939edcb0
JL <115737916+yikesboy@users.noreply.github.com>
2025-08-24 16:00:22 +0200

    feat: defined Keywords

---
+++ Sources/Keywords.swift
@@ -0,0 +1,20 @@
+struct Keywords {
+    static let map: [String: TokenType] = [
+        "and": .AND,
+        "class": .CLASS,
+        "else": .ELSE,
+        "false": .FALSE,
+        "fun": .FUN,
+        "for": .FOR,
+        "if": .IF,
+        "nil": .NIL,
+        "or": .OR,
+        "print": .PRINT,
+        "return": .RETURN,
+        "super": .SUPER,
+        "this": .THIS,
+        "true": .TRUE,
+        "var": .VAR,
+        "while": .WHILE,
+    ]
+}
