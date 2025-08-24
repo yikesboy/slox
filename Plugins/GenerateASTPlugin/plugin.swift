c28d58106fcaa1591ac01dcc87cb898b8501ca35
JL <115737916+yikesboy@users.noreply.github.com>
2025-08-24 19:55:24 +0200

    feat: defined GenerateASTPlugin Build Tool Plugin

---
+++ Plugins/GenerateASTPlugin/plugin.swift
@@ -0,0 +1,24 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct GenerateASTPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let inputFile = context.package.directoryURL.appending(path: "ast-definitions.yml")
+        let outputDir = context.pluginWorkDirectoryURL.appending(path: "Generated")
+        let outputFile = outputDir.appending(path: "AST.swift")
+
+        return [
+            .buildCommand(
+                displayName: "Generating AST Structs",
+                executable: try context.tool(named: "ASTGenerator").url,
+                arguments: [
+                    inputFile.path,
+                    outputDir.path,
+                ],
+                inputFiles: [inputFile],
+                outputFiles: [outputFile]
+            )
+        ]
+    }
+}
