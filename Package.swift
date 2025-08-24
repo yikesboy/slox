685019a3d199618fce5d03c52e184444a7a69109
JL <115737916+yikesboy@users.noreply.github.com>
2025-08-24 23:20:09 +0200

    build: added Yams dependency to ASTGenerator target

--- Package.swift
+++ Package.swift
@@ -7,13 +7,19 @@ let package = Package(
     products: [
         .executable(name: "slox", targets: ["slox"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/jpsim/Yams.git", from: "6.0.1")
+    ],
     targets: [
         .executableTarget(
             name: "slox",
             plugins: ["GenerateASTPlugin"]
         ),
         .executableTarget(
-            name: "ASTGenerator"
+            name: "ASTGenerator",
+            dependencies: [
+                .product(name: "Yams", package: "Yams")
+            ],
         ),
         .plugin(
             name: "GenerateASTPlugin",
