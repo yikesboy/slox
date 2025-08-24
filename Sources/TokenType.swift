5170cc6c5c4d070fc861d69fa8d3c93853bd8372
JL <115737916+yikesboy@users.noreply.github.com>
2025-08-24 15:59:37 +0200

    feat: defined token types

---
+++ Sources/TokenType.swift
@@ -0,0 +1,44 @@
+enum TokenType {
+    case LEFT_PAREN
+    case RIGHT_PAREN
+    case LEFT_BRACE
+    case RIGHT_BRACE
+    case COMMA
+    case DOT
+    case MINUS
+    case PLUS
+    case SEMICOLON
+    case SLASH
+    case STAR
+
+    case BANG
+    case BANG_EQUAL
+    case EQUAL
+    case EQUAL_EQUAL
+    case GREATER
+    case GREATER_EQUAL
+    case LESS
+    case LESS_EQUAL
+    case IDENTIFIER
+    case STRING
+    case NUMBER
+
+    case AND
+    case CLASS
+    case ELSE
+    case FALSE
+    case FUN
+    case FOR
+    case IF
+    case NIL
+    case OR
+    case PRINT
+    case RETURN
+    case SUPER
+    case THIS
+    case TRUE
+    case VAR
+    case WHILE
+
+    case EOF
+}
