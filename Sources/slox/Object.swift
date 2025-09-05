6d0e234d3ede36721be2148c609d052b4538a7a6
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-05 23:59:49 +0200

    refactor: updated object enum

--- Sources/slox/Object.swift
+++ Sources/slox/Object.swift
@@ -1,15 +1,28 @@
-enum Object {
+indirect enum Object: Equatable {
     case String(Swift.String)
     case Number(Double)
     case Boolean(Bool)
     case Nil
 
-    var description: Swift.String {
+    var typeName: Swift.String {
+        let fullDescription = Swift.String(describing: self)
+        return fullDescription.components(separatedBy: "(").first ?? "Unknown"
+    }
+
+    var toString: Swift.String {
         switch self {
         case .String(let value): return value
         case .Number(let value): return Swift.String(value)
         case .Boolean(let value): return Swift.String(value)
         case .Nil: return "nil"
+        }
+    }
+
+    var isTruthy: Bool {
+        switch self {
+        case .Boolean(let value): return value
+        case .Nil: return false
+        default: return true
         }
     }
 }
