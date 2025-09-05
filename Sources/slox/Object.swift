c5f7bdf943f84a568401208bfded04e3ea7a317a
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-05 17:31:31 +0200

    fix: resolved String ambiguity

--- Sources/slox/Object.swift
+++ Sources/slox/Object.swift
@@ -1,6 +1,15 @@
 enum Object {
-    case String(String)
+    case String(Swift.String)
     case Number(Double)
     case Boolean(Bool)
     case Nil
+
+    var description: Swift.String {
+        switch self {
+        case .String(let value): return value
+        case .Number(let value): return Swift.String(value)
+        case .Boolean(let value): return Swift.String(value)
+        case .Nil: return "nil"
+        }
+    }
 }
