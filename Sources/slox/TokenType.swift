1d6d37054a36476337fc15b63eda17259a731bab
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-06 13:06:02 +0200

    feat: temporarily provides exhaustive switch statements

--- Sources/slox/TokenType.swift
+++ Sources/slox/TokenType.swift
@@ -41,4 +41,38 @@ enum TokenType {
     case WHILE
 
     case EOF
+
+    var asBinaryOp: BinaryOperator? {
+        switch self {
+        case .MINUS: return .MINUS
+        case .PLUS: return .PLUS
+        case .STAR: return .STAR
+        case .SLASH: return .SLASH
+        case .GREATER: return .GREATER
+        case .GREATER_EQUAL: return .GREATER_EQUAL
+        case .LESS: return .LESS
+        case .LESS_EQUAL: return .LESS_EQUAL
+        case .BANG_EQUAL: return .BANG_EQUAL
+        case .EQUAL_EQUAL: return .EQUAL_EQUAL
+        default: return nil
+        }
+    }
+
+    var asUnaryOp: UnaryOperator? {
+        switch self {
+        case .MINUS: return .MINUS
+        case .BANG: return .BANG
+        default: return nil
+        }
+    }
+}
+
+// NOTE: replace with more expressive types later on
+
+enum BinaryOperator: CaseIterable {
+    case MINUS, PLUS, STAR, SLASH, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL, BANG_EQUAL, EQUAL_EQUAL
+}
+
+enum UnaryOperator: CaseIterable {
+    case MINUS, BANG
 }
