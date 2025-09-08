fe298a1b1ad0025bfd961ffea56e4ad116b84243
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-08 18:04:31 +0200

    feat: implemented wrapper slox error because swift 6.1 typed throws suck :/

---
+++ Sources/slox/SloxError.swift
@@ -0,0 +1,29 @@
+enum SloxError: ReportableError {
+    case scanner(ScannerError)
+    case parser(ParserError)
+    case runtime(RuntimeError)
+
+    var errorType: String {
+        switch self {
+        case .scanner(let error): return error.errorType
+        case .parser(let error): return error.errorType
+        case .runtime(let error): return error.errorType
+        }
+    }
+
+    var line: Int {
+        switch self {
+        case .scanner(let error): return error.line
+        case .parser(let error): return error.line
+        case .runtime(let error): return error.line
+        }
+    }
+
+    var message: String {
+        switch self {
+        case .scanner(let error): return error.message
+        case .parser(let error): return error.message
+        case .runtime(let error): return error.message
+        }
+    }
+}
