663c279fe1f9657eb308d914fe7def7dde89f5af
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-17 21:35:45 +0200

    feat: parser error definitions

--- Sources/slox/ParserError.swift
+++ Sources/slox/ParserError.swift
@@ -1,7 +1,9 @@
-enum ParserError: ReportableError {
+enum ParserError: ReportableError, @unchecked Sendable {
     case unexpected(Token)
     case expected(expected: TokenType, got: Token)
     case invalidAssignmentTarget(Token)
+    case toManyArguments(token: Token, amount: Int)
+    case toManyParameters(token: Token, amount: Int)
 
     var errorType: String {
         String(describing: Self.self)
@@ -11,7 +13,9 @@ enum ParserError: ReportableError {
         switch self {
         case .unexpected(let token),
             .expected(_, let token),
-            .invalidAssignmentTarget(let token):
+            .invalidAssignmentTarget(let token),
+            .toManyArguments(let token, _),
+            .toManyParameters(let token, _):
             return token.line
         }
     }
@@ -22,6 +26,10 @@ enum ParserError: ReportableError {
             return "Expected \(expectedType) but got '\(gotToken.lexeme)'"
         case .invalidAssignmentTarget(let token):
             return "Invalid assignemnt target '\(token.lexeme)"
+        case .toManyArguments(_, let amount):
+            return "To many arguments: \(amount), maxium: 255"
+        case .toManyParameters(_, let amount):
+            return "To many parameters: \(amount), maxium: 255"
         }
     }
 }
