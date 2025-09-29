724813062a3c4b6093102fe194f86b840fbc0fd5
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-29 20:54:25 +0200

    feat: defined ResolveErrors

--- Sources/slox/ResolverError.swift
+++ Sources/slox/ResolverError.swift
@@ -1,5 +1,7 @@
 enum ResolverError: ReportableError, @unchecked Sendable {
     case cannotReadVariableInItsOwnInit(Token)
+    case variableWithThisNameAlreadyInScope(Token)
+    case cannotReturnFromTopLevelCode(Token)
 
     var errorType: String {
         String(describing: Self.self)
@@ -7,14 +9,21 @@ enum ResolverError: ReportableError, @unchecked Sendable {
 
     var line: Int {
         switch self {
-        case .cannotReadVariableInItsOwnInit(let token): return token.line
+        case .cannotReadVariableInItsOwnInit(let token),
+            .variableWithThisNameAlreadyInScope(let token),
+            .cannotReturnFromTopLevelCode(let token):
+            return token.line
         }
     }
 
     var message: String {
         switch self {
-        case .cannotReadVariableInItsOwnInit:
-            return "Can't read local variable in its own initializer."
+        case .cannotReadVariableInItsOwnInit(let token):
+            return "Can't read local variable '\(token.lexeme)' in its own initializer."
+        case .variableWithThisNameAlreadyInScope(let token):
+            return "Already a variable with name '\(token.lexeme)' in scope."
+        case .cannotReturnFromTopLevelCode:
+            return "Cannot return from top level code."
         }
     }
 }
