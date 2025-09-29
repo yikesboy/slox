b8326a905daedfee3354a5bbdba93b3f5f026ce7
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-29 20:50:46 +0200

    feat: added getAt and assignAt to explicitly specify environement in env chain

--- Sources/slox/Environment.swift
+++ Sources/slox/Environment.swift
@@ -18,6 +18,10 @@ class Environment {
         return enclosing?.get(name: name)
     }
 
+    func getAt(distance: Int, name: Token) -> Object? {
+        return ancestor(distance: distance).values[name.lexeme]
+    }
+
     func assign(name: Token, value: Object) throws(RuntimeError) {
         if values.keys.contains(name.lexeme) {
             values[name.lexeme] = value
@@ -30,5 +34,21 @@ class Environment {
         }
 
         throw .undefinedVariable(name)
+    }
+
+    func assignAt(distance: Int, name: Token, value: Object) {
+        ancestor(distance: distance).values[name.lexeme] = value
+    }
+
+    private func ancestor(distance: Int) -> Environment {
+        var environment: Environment = self
+
+        for _ in 0..<distance {
+            if let enclosingEnv = environment.enclosing {
+                environment = enclosingEnv
+            }
+        }
+
+        return environment
     }
 }
