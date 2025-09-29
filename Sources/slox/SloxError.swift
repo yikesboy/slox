c9d1e89f1e16cb051ad24859b0defb38a4d96bb3
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-29 20:51:35 +0200

    feat: extended SloxError wrapper with ResolverErrors

--- Sources/slox/SloxError.swift
+++ Sources/slox/SloxError.swift
@@ -1,12 +1,14 @@
 enum SloxError: ReportableError {
     case scanner(ScannerError)
     case parser(ParserError)
+    case resolver(ResolverError)
     case runtime(RuntimeError)
 
     var errorType: String {
         switch self {
         case .scanner(let error): return error.errorType
         case .parser(let error): return error.errorType
+        case .resolver(let error): return error.errorType
         case .runtime(let error): return error.errorType
         }
     }
@@ -15,6 +17,7 @@ enum SloxError: ReportableError {
         switch self {
         case .scanner(let error): return error.line
         case .parser(let error): return error.line
+        case .resolver(let error): return error.line
         case .runtime(let error): return error.line
         }
     }
@@ -23,6 +26,7 @@ enum SloxError: ReportableError {
         switch self {
         case .scanner(let error): return error.message
         case .parser(let error): return error.message
+        case .resolver(let error): return error.message
         case .runtime(let error): return error.message
         }
     }
