51fdb27d714a861c1d31aa4ae0dbf3ff3ff033b4
JL <115737916+yikesboy@users.noreply.github.com>
2025-09-30 19:45:39 +0200

    feat: Slox class instance

--- Sources/slox/SloxInstance.swift
+++ Sources/slox/SloxInstance.swift
@@ -1,3 +1,20 @@
-struct SloxInstance: Equatable {
+class SloxInstance: Equatable {
     let _class: SloxClass
+    var fields = [String: Object]()
+
+    init(_class: SloxClass) {
+        self._class = _class
+    }
+
+    func get(name: Token) -> Object? {
+        return fields[name.lexeme]
+    }
+
+    func set(name: Token, value: Object) {
+        fields[name.lexeme] = value
+    }
+
+    static func == (lhs: SloxInstance, rhs: SloxInstance) -> Bool {
+        return lhs._class == rhs._class && lhs.fields == rhs.fields
+    }
 }
